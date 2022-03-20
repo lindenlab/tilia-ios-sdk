a136bfaa90c58cfc53853661a3b1866244bb3c71
Serhii Petrishenko <serhii.petrishenko@innovecs.com>
2022-03-20 13:41:09 +0200

    Initial Commit

---
+++ TiliaSDK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TiliaSDK
+//
+//  Created by Serhii.Petrishenko on 20.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
