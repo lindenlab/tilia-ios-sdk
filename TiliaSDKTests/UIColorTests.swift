8908106ab758bc021ff389290a99f5828d0fc838
BMacLinden <bmac@lindenlab.com>
2022-04-20 08:51:42 -0700

    reverting fail test check

--- TiliaSDKTests/UIColorTests.swift
+++ TiliaSDKTests/UIColorTests.swift
@@ -13,8 +13,7 @@ class UIColorTests: XCTestCase {
   func testIsColorDark() {
     let whiteColor = UIColor.white
     let blackColor = UIColor.black
-//    XCTAssertFalse(whiteColor.isColorDark())
-      XCTAssertTrue(whiteColor.isColorDark()) // test for making sure github actions unit tests are runnning as expected
+    XCTAssertFalse(whiteColor.isColorDark())
     XCTAssertTrue(blackColor.isColorDark())
   }
   
