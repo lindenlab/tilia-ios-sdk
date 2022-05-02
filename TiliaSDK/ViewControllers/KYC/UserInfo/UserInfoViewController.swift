4a22c97318bc5e41611d87a300413ba9ea8e0046
Serhii Petrishenko <serhii.petrishenko@innovecs.com>
2022-05-02 13:41:58 +0300

    Removed files

--- TiliaSDK/ViewControllers/KYC/UserInfo/UserInfoViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  UserInfoViewController.swift
-//  TiliaSDK
-//
-//  Created by Serhii.Petrishenko on 02.05.2022.
-//
-
-import Foundation
--- TiliaSDK/ViewControllers/KYC/UserInfo/UserInfoViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  UserInfoViewModel.swift
-//  TiliaSDK
-//
-//  Created by Serhii.Petrishenko on 02.05.2022.
-//
-
-import Foundation
