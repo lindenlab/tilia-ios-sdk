3860a1628473ebac199f6f1343632d6be1e2129e
Serhii Petrishenko <serhii.petrishenko@innovecs.com>
2022-05-11 23:58:59 +0300

    Added keyboard impr

--- TiliaSDK/Extensions/UIView+Helpers.swift
+++ TiliaSDK/Extensions/UIView+Helpers.swift
@@ -9,6 +9,18 @@ import UIKit
 
 extension UIView {
   
+  var firstResponder: UIView? {
+    if self.isFirstResponder {
+      return self
+    }
+    for view in self.subviews {
+      if let firstResponder = view.firstResponder {
+        return firstResponder
+      }
+    }
+    return nil
+  }
+  
   func addClosingKeyboardOnTap() {
     let closeTap = UITapGestureRecognizer(target: self, action: #selector(UIView.endEditing(_:)))
     closeTap.cancelsTouchesInView = false
