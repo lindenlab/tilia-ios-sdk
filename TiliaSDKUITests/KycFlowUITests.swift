f80c0b5ea15d84f8c4dcb4a0fbd0288608207107
Serhii Petrishenko <serhii.petrishenko@innovecs.com>
2022-07-25 14:29:25 +0300

    Minor impr

---
+++ TiliaSDKUITests/KycFlowUITests.swift
@@ -0,0 +1,16 @@
+//
+//  KycFlowUITests.swift
+//  TiliaSDKUITests
+//
+//  Created by Serhii.Petrishenko on 25.07.2022.
+//
+
+import XCTest
+
+final class KycFlowUITests: XCTestCase {
+  
+  override func setUpWithError() throws {
+    continueAfterFailure = false
+  }
+
+}
