64445dbb53c737d6c4efac4d9bba6803a06bd94b
Serhii Petrishenko <serhii.petrishenko@innovecs.com>
2022-08-14 12:29:17 +0300

    Minor impr

--- TiliaSDK/ViewControllers/CustomUI/NonPrimaryButton.swift
+++ TiliaSDK/ViewControllers/CustomUI/NonPrimaryButton.swift
@@ -10,7 +10,7 @@ import UIKit
 final class NonPrimaryButton: ButtonWithBackgroundColor {
   
   override var intrinsicContentSize: CGSize {
-    return CGSize(width: super.layoutFittingExpandedSize.width, height: 44)
+    return CGSize(width: super.intrinsicContentSize.width, height: 44)
   }
   
   override init(frame: CGRect) {
