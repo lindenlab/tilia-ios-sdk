3f0e9e1265c0bb3e000b9d6f44785fc01975347d
Serhii Petrishenko <serhii.petrishenko@innovecs.com>
2022-08-25 17:30:38 +0300

    Minor impr

--- TiliaSDK/Extensions/UIColor+Colors.swift
+++ TiliaSDK/Extensions/UIColor+Colors.swift
@@ -136,9 +136,9 @@ extension UIColor {
     let r = Int(color >> 16) & mask
     let g = Int(color >> 8) & mask
     let b = Int(color) & mask
-    let red   = CGFloat(r) / 255.0
+    let red = CGFloat(r) / 255.0
     let green = CGFloat(g) / 255.0
-    let blue  = CGFloat(b) / 255.0
+    let blue = CGFloat(b) / 255.0
     self.init(red: red, green: green, blue: blue, alpha: alpha)
   }
   
