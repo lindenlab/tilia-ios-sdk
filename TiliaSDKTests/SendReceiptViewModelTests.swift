007c85291b0e4226cec70556f21d013fa439948e
Serhii Petrishenko <serhii.petrishenko@innovecs.com>
2022-09-02 14:36:47 +0300

    Added test file

---
+++ TiliaSDKTests/SendReceiptViewModelTests.swift
@@ -0,0 +1,20 @@
+//
+//  SendReceiptViewModelTests.swift
+//  TiliaSDKTests
+//
+//  Created by Serhii.Petrishenko on 02.09.2022.
+//
+
+import XCTest
+import Combine
+@testable import TiliaSDK
+
+final class SendReceiptViewModelTests: XCTestCase {
+  
+  var subscriptions: Set<AnyCancellable>!
+  
+  override func setUpWithError() throws {
+    subscriptions = []
+  }
+  
+}
