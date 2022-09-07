a8c5e1b4a0074555abea7bfac2d57bacd24f96b6
Serhii Petrishenko <serhii.petrishenko@innovecs.com>
2022-09-07 17:47:14 +0300

    Added file

---
+++ TiliaSDKUITests/TransactionDetailsFlowUITests.swift
@@ -0,0 +1,16 @@
+//
+//  TransactionDetailsFlowUITests.swift
+//  TiliaSDKUITests
+//
+//  Created by Serhii.Petrishenko on 07.09.2022.
+//
+
+import XCTest
+
+final class TransactionDetailsFlowUITests: XCTestCase {
+  
+  override func setUpWithError() throws {
+    continueAfterFailure = false
+  }
+  
+}
