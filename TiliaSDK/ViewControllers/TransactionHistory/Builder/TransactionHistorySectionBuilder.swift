3a422af099a8368fe174552a5ef46e2f539c47df
Serhii Petrishenko <serhii.petrishenko@innovecs.com>
2022-10-05 23:46:08 +0300

    Added new logic

--- TiliaSDK/ViewControllers/TransactionHistory/Builder/TransactionHistorySectionBuilder.swift
+++ TiliaSDK/ViewControllers/TransactionHistory/Builder/TransactionHistorySectionBuilder.swift
@@ -54,11 +54,11 @@ struct TransactionHistoryPendingSectionBuilder: TransactionHistorySectionBuilder
   
   func updateSections(with items: [TransactionDetailsModel], oldLastItem: TransactionDetailsModel?, sections: inout [TransactionHistorySectionModel]) -> TableUpdate {
     var insertRows: [IndexPath] = []
-    if sections.isEmpty {
+    if oldLastItem == nil {
       sections.append(.init(header: nil, items: []))
     }
     let lastItemIndex = sections[0].items.count - 1
-    if lastItemIndex >= 0, sections[0].items[lastItemIndex].isLast {
+    if lastItemIndex >= 0 {
       sections[0].items[lastItemIndex].isLast = false
     }
     let count = items.count
@@ -78,24 +78,26 @@ struct TransactionHistoryHistorySectionBuilder: TransactionHistorySectionBuilder
   
   func updateSections(with items: [TransactionDetailsModel], oldLastItem: TransactionDetailsModel?, sections: inout [TransactionHistorySectionModel]) -> TableUpdate {
     let oldLastSectionIndex = sections.count - 1
+    var lastSectionIndex = oldLastSectionIndex
+    var lastItemIndex = -1
+    if lastSectionIndex >= 0 && sections[lastSectionIndex].items.count - 1 >= 0 {
+      lastItemIndex = sections[lastSectionIndex].items.count - 1
+      sections[lastSectionIndex].items[lastItemIndex].isLast = false
+    }
     var lastItem = oldLastItem
     var insertRows: [IndexPath] = []
     items.enumerated().forEach { index, item in
       let isItemLast = self.isLast(in: items, index: index)
       if let lastItem = lastItem, lastItem.transactionDate.getDateDiff(for: item.transactionDate) == 0 {
-        let lastSectionIndex = sections.count - 1
-        let lastItemIndex = sections[lastSectionIndex].items.count - 1
         if lastSectionIndex == oldLastSectionIndex {
-          insertRows.append(.init(row: lastItemIndex + 1,
+          insertRows.append(.init(row: lastItemIndex + index + 1,
                                   section: lastSectionIndex))
-        }
-        if lastItemIndex >= 0, sections[lastSectionIndex].items[lastItemIndex].isLast {
-          sections[lastSectionIndex].items[lastItemIndex].isLast = false
         }
         sections[lastSectionIndex].items.append(self.item(for: item,
                                                           isLast: isItemLast,
                                                           sectionType: .history))
       } else {
+        lastSectionIndex += 1
         let title = item.transactionDate.string(formatter: .longDateFormatter)
         sections.append(.init(header: .init(title: title, value: nil),
                               items: [self.item(for: item,
@@ -103,15 +105,15 @@ struct TransactionHistoryHistorySectionBuilder: TransactionHistorySectionBuilder
                                                 sectionType: .history)]))
       }
       if isItemLast {
-        let count = String(sections[sections.count - 1].items.count)
+        let count = String(sections[lastSectionIndex].items.count)
         let value = L.total(with: count).attributedString(font: .systemFont(ofSize: 12),
                                                           color: .tertiaryTextColor,
                                                           subStrings: (count, .boldSystemFont(ofSize: 12), .tertiaryTextColor))
-        sections[sections.count - 1].header?.value = value
+        sections[lastSectionIndex].header?.value = value
       }
       lastItem = item
     }
-    let insertSectionsRange = oldLastSectionIndex + 1..<sections.count
+    let insertSectionsRange = oldLastSectionIndex + 1...lastSectionIndex
     let insertSections = insertSectionsRange.isEmpty ? nil : IndexSet(integersIn: insertSectionsRange)
     return (insertSections, insertRows.isEmpty ? nil : insertRows)
   }
