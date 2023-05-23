6271f165a6293682734186808365c96bbd30e606
Serhii Petrishenko <serhii.petrishenko@innovecs.com>
2023-05-23 14:08:42 +0300

    Added new test

--- TiliaSDKTests/Kyc/UserInfoViewModelTests.swift
+++ TiliaSDKTests/Kyc/UserInfoViewModelTests.swift
@@ -248,4 +248,74 @@ final class UserInfoViewModelTests: XCTestCase {
     XCTAssertNotNil(errorCallback)
   }
   
+  func testSuccessLoadContentAndUpdateEmail() {
+    var updateCallback: TLUpdateCallback?
+    var content: Void?
+    var didStartEditingEmail: UserInfoEditEmail?
+    var verifyEmail: Void?
+    var emailVerified: UserInfoEmailVerified?
+    
+    let updateCallbackExpectation = XCTestExpectation(description: "testSuccessLoadContentAndUpdateEmail_UpdateCallback")
+    let networkManager = NetworkManager(serverClient: ServerTestClient())
+    let viewModel = UserInfoViewModel(manager: networkManager,
+                                      onUpdate: { updateCallback = $0; updateCallbackExpectation.fulfill() },
+                                      onComplete: nil,
+                                      onError: nil)
+    
+    let contentExpectation = XCTestExpectation(description: "testSuccessLoadContentAndUpdateEmail_ContentExpectation")
+    viewModel.content.sink { [weak viewModel] _ in
+      content = ()
+      viewModel?.startEditingEmail(at: 0)
+      contentExpectation.fulfill()
+    }.store(in: &subscriptions)
+    
+    let didStartEditingEmailExpectation = XCTestExpectation(description: "testSuccessLoadContentAndUpdateEmail_DidStartEditingEmail")
+    viewModel.didStartEditingEmail.sink { [weak viewModel] in
+      didStartEditingEmail = $0
+      let item = UserInfoSectionBuilder.Section.Item(mode: .fields(.init(type: .email,
+                                                                         fields: [.init(accessibilityIdentifier: nil)])),
+                                                     title: nil,
+                                                     description: nil)
+      let section = UserInfoSectionBuilder.Section(type: .email,
+                                                   mode: .normal,
+                                                   isFilled: false,
+                                                   items: [item])
+      viewModel?.setText("test@gmail.com", for: section, indexPath: .init(row: 0, section: 0), fieldIndex: 0)
+      viewModel?.updateEmail(at: $0.index)
+      didStartEditingEmailExpectation.fulfill()
+    }.store(in: &subscriptions)
+    
+    let verifyEmailExpectation = XCTestExpectation(description: "testSuccessLoadContentAndUpdateEmail_VerifyEmail")
+    viewModel.verifyEmail.sink { [weak viewModel] in
+      verifyEmail = ()
+      viewModel?.onEmailVerified(viewModel?.verifyEmailMode ?? .update)
+      verifyEmailExpectation.fulfill()
+    }.store(in: &subscriptions)
+    
+    let emailVerifiedExpectation = XCTestExpectation(description: "testSuccessLoadContentAndUpdateEmail_EmailVerified")
+    viewModel.emailVerified.sink {
+      emailVerified = $0
+      emailVerifiedExpectation.fulfill()
+    }.store(in: &subscriptions)
+    
+    let expectations = [
+      updateCallbackExpectation,
+      contentExpectation,
+      didStartEditingEmailExpectation,
+      verifyEmailExpectation,
+      emailVerifiedExpectation
+    ]
+    
+    TLManager.shared.setToken(UUID().uuidString)
+    viewModel.load()
+    
+    wait(for: expectations, timeout: 2)
+    
+    XCTAssertEqual(updateCallback?.event.action, .emailVerified)
+    XCTAssertNotNil(content)
+    XCTAssertEqual(didStartEditingEmail?.index, 0)
+    XCTAssertNotNil(verifyEmail)
+    XCTAssertNotNil(emailVerified)
+  }
+  
 }
